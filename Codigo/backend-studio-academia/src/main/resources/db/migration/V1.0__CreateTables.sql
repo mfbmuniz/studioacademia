d4bc4709a8d787da0811e09cbce87c43af2a4b4e
Marcos <filips8@hotmail.com>
2022-09-18 14:55:26 -0300

    banco criado

--- Codigo/backend-studio-academia/src/main/resources/db/migration/V1.0__CreateTables.sql
+++ Codigo/backend-studio-academia/src/main/resources/db/migration/V1.0__CreateTables.sql
@@ -11,6 +11,22 @@ CREATE TABLE states (
                         uf VARCHAR(2) NOT NULL
 );
 
+CREATE SEQUENCE plans_seq;
+CREATE TABLE plans (
+                    plans_id INT DEFAULT NEXTVAL ('plans_seq') PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL,
+                    price NUMERIC NOT NULL,
+                    description TEXT NOT NULL,
+                    planCode VARCHAR(3) NOT NULL,
+                    contracted_days INT NOT NULL,
+
+
+                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL
+
+
+);
+
 CREATE SEQUENCE address_seq;
 CREATE TABLE address(
                         address_id INT DEFAULT NEXTVAL ('address_seq') PRIMARY KEY,
@@ -34,12 +50,12 @@ CREATE TABLE users (
                        sex VARCHAR(1) NOT NULL,
                        legal_document VARCHAR(11) NOT NULL,
                        address_id INT NOT NULL,
-                       plans_id INT CHECK (user_id > 0) ,
+                       plans_id INT CHECK (plans_id > 0) ,
 
                        created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
                        deleted_at TIMESTAMP(0) NULL DEFAULT NULL,
 
-                       CONSTRAINT FK_address_id FOREIGN KEY (address_id) REFERENCES address(address_id)
+                       CONSTRAINT FK_address_id FOREIGN KEY (address_id) REFERENCES address(address_id),
                        CONSTRAINT FK_plans_id FOREIGN KEY (plans_id) REFERENCES plans(plans_id)
 );
 
@@ -97,29 +113,19 @@ CREATE TABLE admin_message_service (
 
 );
 
-CREATE SEQUENCE plans_seq;
-CREATE TABLE plans (
-                    plans_id INT DEFAULT NEXTVAL ('plans_seq') PRIMARY KEY,
-                    name VARCHAR(50) NOT NULL,
-                    price NUMERIC NOT NULL,
-                    description TEXT NOT NULL,
-                    planCode VARCHAR(3) NOT NULL,
-                    contracted_days INT NOT NULL,
-                    week_days_id INT CHECK (user_id > 0),
 
-                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
-                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL,
-
-                    CONSTRAINT FK_week_days_id FOREIGN KEY (week_days_id) REFERENCES week_days(week_days_id)
-
-
-);
 
 CREATE SEQUENCE week_days_seq;
 CREATE TABLE week_days (
                     week_days_id INT DEFAULT NEXTVAL ('week_days_seq') PRIMARY KEY,
                     user_id INT CHECK (user_id > 0),
-                    daysOfWeek varchar (30),
+                    sunday BOOLEAN,
+                    monday BOOLEAN,
+                    tuesday BOOLEAN,
+                    wednesday BOOLEAN,
+                    thursday BOOLEAN,
+                    friday BOOLEAN,
+                    saturday BOOLEAN,
 
 
                     created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
@@ -141,25 +147,13 @@ CREATE TABLE calendar (
 
 );
 
-CREATE SEQUENCE calendar_seq;
-CREATE TABLE calendar (
-                    calendar_id INT DEFAULT NEXTVAL ('calendar_seq') PRIMARY KEY,
-                    dateEvent TIMESTAMP(0) NULL DEFAULT NULL,
-                    dateDescription TEXT NOT NULL,
-
-
-                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
-                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL
-
-);
 
 CREATE SEQUENCE monthly_payment_seq;
 CREATE TABLE monthly_payment (
-                    monthly_payment INT DEFAULT NEXTVAL ('monthly_payment') PRIMARY KEY,
+                    monthly_payment_id INT DEFAULT NEXTVAL ('monthly_payment_seq') PRIMARY KEY,
                     due_Date TIMESTAMP(0) NULL DEFAULT NULL,
                     payment_CHECK BOOLEAN  not null,
-
-                    dateDescription TEXT NOT NULL,
+                    user_id INT CHECK (user_id > 0),
 
 
                     created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
@@ -168,3 +162,116 @@ CREATE TABLE monthly_payment (
                     CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
 
 );
+CREATE SEQUENCE exercises_seq;
+CREATE TABLE exercises (
+                    exercises_id INT DEFAULT NEXTVAL ('exercises_seq ') PRIMARY KEY,
+                    exercise_name VARCHAR(100)  not null,
+                    exercise_link VARCHAR(100)  not null,
+
+
+                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL
+
+);
+
+CREATE SEQUENCE user_file_seq;
+CREATE TABLE user_file (
+                    user_file_id INT DEFAULT NEXTVAL ('user_file_seq') PRIMARY KEY,
+                    user_id INT CHECK (user_id > 0) not null,
+
+                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL,
+
+                    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+
+);
+
+CREATE SEQUENCE user_exercises_seq;
+CREATE TABLE user_exercises (
+                    user_exercises_id INT DEFAULT NEXTVAL ('user_exercises_seq') PRIMARY KEY,
+                    user_file_id INT CHECK (user_file_id > 0) not null,
+                    exercises_id INT CHECK (exercises_id > 0) not null,
+
+
+                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL,
+
+                    CONSTRAINT FK_user_file_id FOREIGN KEY (user_file_id) REFERENCES user_file(user_file_id),
+                    CONSTRAINT FK_exercises_id FOREIGN KEY (exercises_id) REFERENCES exercises(exercises_id)
+
+);
+
+
+
+CREATE SEQUENCE physical_assessment_seq;
+CREATE TABLE physical_assessment (
+                    physical_assessment_id INT DEFAULT NEXTVAL ('physical_assessment_seq') PRIMARY KEY,
+                    user_id INT CHECK (user_id > 0) ,
+                    height DECIMAL,
+                    general_description DECIMAL,
+                    ideal_weight DECIMAL,
+                    actual_weight DECIMAL,
+                    relaxed_right_arm DECIMAL,
+                    relaxed_left_arm DECIMAL,
+                    contracted_right_arm DECIMAL,
+                    contracted_left_arm DECIMAL,
+                    right_forearm DECIMAL,
+                    left_forearm DECIMAL,
+                    right_fist DECIMAL,
+                    left_fist DECIMAL,
+                    neck DECIMAL,
+                    chest DECIMAL,
+                    abdomen DECIMAL,
+                    shoulder DECIMAL,
+                    waist DECIMAL,
+                    hip DECIMAL,
+                    right_calf DECIMAL,
+                    left_calf DECIMAL,
+                    right_thigh DECIMAL,
+                    left_thigh DECIMAL,
+                    right_proximal_thigh DECIMAL,
+                    left_proximal_thigh DECIMAL,
+                    bone_fist_diameter DECIMAL,
+                    femur DECIMAL,
+                    skin_folds_biceps DECIMAL,
+                    skin_folds_triceps DECIMAL,
+                    skin_folds_abdominal DECIMAL,
+                    skin_folds_suprailiac DECIMAL,
+                    skin_folds_medium_armpit DECIMAL,
+                    skin_folds_chest DECIMAL,
+                    skin_folds_subscapularis DECIMAL,
+                    skin_folds_thigh DECIMAL,
+                    skin_medial_calf DECIMAL,
+                    IMC DECIMAL,
+                    fat_mass DECIMAL,
+                    corporal_water DECIMAL,
+                    residual_weight DECIMAL,
+                    visceral_fat DECIMAL,
+                    lean_mass DECIMAL,
+                    muscular_weight DECIMAL,
+                    metabolic_age DECIMAL,
+                    bone_weight DECIMAL,
+                    physical_assessment_date DECIMAL,
+
+                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL,
+
+                    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+
+);
+
+CREATE SEQUENCE user_assessments_seq;
+CREATE TABLE user_assessments (
+                    user_assessments_id INT DEFAULT NEXTVAL ('user_assessments_seq') PRIMARY KEY,
+                    user_id INT CHECK (user_id > 0) not null,
+                    physical_assessment_id INT CHECK (physical_assessment_id > 0) not null,
+                    general_description varchar(250),
+
+                    created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+                    deleted_at TIMESTAMP(0) NULL DEFAULT NULL,
+
+                    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+                    CONSTRAINT FK_physical_assessment_id FOREIGN KEY (physical_assessment_id) REFERENCES physical_assessment(physical_assessment_id)
+
+);
+
