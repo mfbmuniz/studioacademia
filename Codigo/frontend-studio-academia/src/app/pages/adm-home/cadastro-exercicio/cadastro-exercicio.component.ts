da2f3a399c1f444d02fa335b927fb2a742536535
Arthur Alexi <arthuralexi@hotmail.com>
2022-09-25 18:15:01 -0300

    Update ts formExercicios

--- Codigo/frontend-studio-academia/src/app/pages/adm-home/cadastro-exercicio/cadastro-exercicio.component.ts
+++ Codigo/frontend-studio-academia/src/app/pages/adm-home/cadastro-exercicio/cadastro-exercicio.component.ts
@@ -27,7 +27,33 @@ export class CadastroExercicioComponent implements OnInit {
   }
 
   public cadastrar() : void{
+    let body = {
+      name : this.novoExercicioForm.value("name"),
+      description : this.novoExercicioForm.value("description"),
+      url : this.novoExercicioForm.value("url")
+    }
 
   }
 
 }
+/**
+ * let body = {
+      email: this.novoAlunoForm.value["email"],
+      password: this.novoAlunoForm.value["password"],
+      passwordConfirm: this.novoAlunoForm.value["passwordConfirm"],
+      name: this.novoAlunoForm.value["name"],
+      legalDocument: this.novoAlunoForm.value["legal_document"],
+      phone: this.novoAlunoForm.value["phone"],
+      birthDate: this.novoAlunoForm.value["birthDate"],
+      roles: this.novoAlunoForm.value["roles"],
+      sex: this.novoAlunoForm.value["sex"],
+      address: {
+        zipCode: this.novoAlunoForm.value["zipCode"],
+        street: this.novoAlunoForm.value["street"],
+        number: this.novoAlunoForm.value["number"],
+        district: this.novoAlunoForm.value["district"],
+        complement: this.novoAlunoForm.value["complement"],
+        state: this.novoAlunoForm.value["state"],
+        city: this.novoAlunoForm.value["city"],
+      }
+ */
