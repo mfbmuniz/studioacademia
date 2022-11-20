e77eb68ee7edbd8266c44e7f6350c33ea68bbb12
Marcos Felipe <filips8@hotmail.com>
2022-11-20 17:41:32 -0300

    controller das avaliações fisicas concluido e rotas inseridas no front

--- Codigo/frontend-studio-academia/src/app/services/physical-assessmentService..ts
+++ Codigo/frontend-studio-academia/src/app/services/physical-assessmentService..ts
@@ -12,7 +12,7 @@ export class PhysicalAssessmentService {
   private static API_URLS = {
     CREATE: `${environment.apiUrl}/physical-assessment/create`,
     UPDATE : `${environment.apiUrl}/physical-assessment/edit`,
-    DELETE : `${environment.apiUrl}/physical-assessment/delete/planId`,
+    DELETE : `${environment.apiUrl}/physical-assessment/delete/`,
   };
 
   create(body : any){
@@ -23,15 +23,19 @@ export class PhysicalAssessmentService {
     return this.http.post(PhysicalAssessmentService.API_URLS.UPDATE,body)
   }
 
-  delete(planId : number){
-    return this.http.delete(`${PhysicalAssessmentService.API_URLS.DELETE}/${planId}`)
+  delete(physicalAssessmentId : number){
+    return this.http.delete(`${PhysicalAssessmentService.API_URLS.DELETE}physicalAssessmentId/${physicalAssessmentId}`)
   }
 
   listPhysicalAssessment(page : number , size : number){
     return this.http.get(`${environment.apiUrl}/physical-assessment/page/${page}/size/${size}`)
   }
 
-  listPhysicalAssessmentByName(page : number , size : number, name : string){
-    return this.http.get(`${environment.apiUrl}/physical-assessment/page/${page}/size/${size}/name/${name}`)
+  listPhysicalAssessmentByName(page : number , size : number, idUser : string){
+    return this.http.get(`${environment.apiUrl}/physical-assessment/page/${page}/size/${size}/idUser/${idUser}`)
+  }
+
+  listPhysicalAssessmentByProfessional(page : number , size : number, professionalId : string){
+    return this.http.get(`${environment.apiUrl}/physical-assessment/page/${page}/size/${size}/professionalId/${professionalId}`)
   }
 }
